b382401a84c38c011b9684ef263297c14cc09acd
westRegion-gif <salehsaeedalmansoori@gmail.com>
2026-08-30 02:43:33 +0400

    Expose safe customer order lookup endpoint

--- lmtd/apps/api/src/orders/orders.controller.ts
+++ lmtd/apps/api/src/orders/orders.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { OrdersService } from './orders.service';
 
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly orders: OrdersService) {}
+
+  @Get(':id')
+  get(@Param('id') id: string) {
+    return this.orders.getPublic(id);
+  }
 
   @Post()
   create(@Body() dto: CreateOrderDto) {
