a8fd79c8d2468f73ba453e1d32e5db5ca68da7e0
westRegion-gif <salehsaeedalmansoori@gmail.com>
2026-08-29 17:52:41 +0400

    Register public and admin media controllers

--- lmtd/apps/api/src/media/media.module.ts
+++ lmtd/apps/api/src/media/media.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AdminAuthModule } from '../admin-auth/admin-auth.module';
-import { MediaController } from './media.controller';
+import { AdminMediaController, PublicMediaController } from './media.controller';
 import { MediaService } from './media.service';
 
-@Module({ imports:[AdminAuthModule], controllers:[MediaController], providers:[MediaService] })
+@Module({
+  imports: [AdminAuthModule],
+  controllers: [PublicMediaController, AdminMediaController],
+  providers: [MediaService],
+})
 export class MediaModule {}
